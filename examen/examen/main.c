3bbab175fc8b71112fdc00b4ff2304cbad5ba442
Guillermo Gonzalez J <a01653930@itesm.mx>
2022-06-07 19:32:50 -0500

    timer 2, sensor interrupt, pwm

--- examen/examen/main.c
+++ examen/examen/main.c
@@ -8,32 +8,35 @@
 #include <xc.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 
-uint8_t f_read = 1, count = 0;
+const char temperatura[] PROGMEM = {0x55};
+
+uint8_t f_read = 0x00, count = 0;
 
 uint8_t valores[3] = {0,0,0};
 
-ISR(INT0_vect);								// Interrupci髇 switch
-ISR(INT1_vect);								// Interrupci髇 sensor 髉tico
-ISR(TIMER0_COMP_vect);						// Interrupci髇 Timer 0 (para RFID)
+ISR(INT0_vect);								// Interrupci贸n switch
+ISR(INT1_vect);								// Interrupci贸n sensor 贸ptico
+ISR(TIMER0_COMP_vect);						// Interrupci贸n Timer 0 (para RFID)
 
 void lectura();
 
 int main(void){
 	/*------ Seteo de puertos ------*/ 
 	DDRD |= (0 << PD2);						// Switch
-	DDRD |= (0 << PD3);						// Sensor 髉tico
+	DDRD |= (0 << PD3);						// Sensor 贸ptico
 	DDRA = 0b00000000;						// RFID
 	
-	DDRB |= (1 << PB3);						// Calefactor (PWM)
-	DDRB |= (1 << PB0);						// Electrov醠vula
-	DDRC |= 0b11111111;					// Display 7 segmentos
+	DDRB |= (1 << PD4);						// Calefactor (PWM)
+	DDRB |= (1 << PB0);						// Electrov谩lvula
+	DDRC |= 0b11111111;						// Display 7 segmentos
 	
 	sei();
 	
 	/*------ Palabras de control ------*/
 	//Interrupciones externas 
-	GICR = (1 << INT1) | (1 << INT2);
+	GICR = (1 << INT0) | (1 << INT1);
 	MCUCR = (1 << ISC00) | (1 << ISC11);
 	
 	//Interrupcion Timer
@@ -43,19 +46,59 @@ int main(void){
 }
 
 ISR(INT0_vect){
-	if(f_read == 1){
+	f_read *= (-1);																	//Negate flag
+	if(f_read == 0xFF){
 		OCR2 = 7;
-		TCCR2 = (1 << WGM21) | (1 << CS22) | (1 << CS21) | (1 << CS20);
-		f_read--;
+		TCCR2 = (1 << WGM21) | (1 << CS22) | (1 << CS21) | (1 << CS20);				//Initialize 100ms Timer
+		TCCR1A = (1 << COM1B1) | (1 << WGM11) |(1 << WGM10);						//Initialize PWM
+		TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);							//Initialize PWM
 	} else{
-		TCCR2 = 0;
-		f_read++;
+		TCCR2 = 0;																	//Turn down 100ms Timer
+		TCCR1A = 0;																	//Stop PWM
+		TCCR1B = 0;																	//Stop PWM
 	}	
 }
 
 ISR(TIMER2_COMP_vect){
-	valores[count] = PORTA;
-	count++;
-	if(count == 2) count = 0;	
+	valores[count] = PINA;
+	if(count == 0){
+		if(valores[count] < 3){
+			PORTB |= (1 << PB0);
+		} else{
+			PORTB &= ~(1 << PB0);
+		}
+		count++;
+	} else if(count == 1){
+		OCR1A = 781;
+		if(valores[count] < temperatura[0]){						//75% PWM
+			OCR1B = 586;
+		} else{														//25% PWM
+			OCR1B = 195;
+		}
+		count++;
+	} else if(count == 2){
+		
+		count = 0;
+	} 
 }
+
+ISR(INT1_vect){
+	TCCR2 = 0;																	//Turn down 100ms Timer
+	TCCR1A = 0;																	//Stop PWM
+	TCCR1B = 0;
+}
+		
+		
+// 		if(valores[count] < temperatura[0]){
+// 			OCR1A = 781;
+// 			OCR1B = 586;
+// 			TCCR1A = (1 << COM1B1) | (1 << WGM11) |(1 << WGM10);
+// 			TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);
+// 		} else{
+// 			OCR1A = 781;
+// 			OCR1B = 195;
+// 			TCCR1A = (1 << COM1B1) | (1 << WGM11) |(1 << WGM10);
+// 			TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);			
+// 		}			
+
 
